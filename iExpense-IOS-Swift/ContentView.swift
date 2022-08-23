1eb59eaadbb6a6e1e9b33bd54fab36ba2457f75a
Steffan Crowley <steffancrow@gmail.com>
2022-08-23 11:14:32 -0700

    Initial Commit

---
+++ iExpense-IOS-Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense-IOS-Swift
+//
+//  Created by steffan crowley on 8/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iExpense-IOS-Swift/iExpense_IOS_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpense_IOS_SwiftApp.swift
+//  iExpense-IOS-Swift
+//
+//  Created by steffan crowley on 8/23/22.
+//
+
+import SwiftUI
+
+@main
+struct iExpense_IOS_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
